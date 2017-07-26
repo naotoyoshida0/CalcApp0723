569c3b5441b1fcfc9e0323919f40010d3ac05a3d
naotoyoshida0 <naotoyoshida16g>
2017-07-27 07:11:19 +0900

    0727errer

--- app/src/main/java/jp/techacademy/yoshida/naoto/calcapp2/MainActivity.java
+++ app/src/main/java/jp/techacademy/yoshida/naoto/calcapp2/MainActivity.java
@@ -3,16 +3,24 @@ package jp.techacademy.yoshida.naoto.calcapp2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import static jp.techacademy.yoshida.naoto.calcapp2.R.id.textView;
 
 public class MainActivity  extends AppCompatActivity implements View.OnClickListener  {
 
     EditText mEditText1;
     EditText mEditText2;
     TextView mTextView;
+
+
+
+    EditText editText1 = (EditText) findViewById(R.id.editText1);
+    EditText editText2 = (EditText) findViewById(R.id.editText2);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +39,7 @@ public class MainActivity  extends AppCompatActivity implements View.OnClickList
         Button button4 = (Button) findViewById(R.id.button4);
         button4.setOnClickListener(this);
 
-        EditText editText1 = (EditText) findViewById(R.id.editText1);
-        EditText editText2 = (EditText) findViewById(R.id.editText2);
+
 
 
 
@@ -40,14 +47,15 @@ public class MainActivity  extends AppCompatActivity implements View.OnClickList
 
 
     @Override
-    public void onClick(View v) {//ボタン１を押した時editText1.2の数値を足して、遷移画面でTOTAL表示
-        Intent intent = new Intent(this, secondActivity.class);                           //遷移先を指定
-        intent.getFloatExtra("TOTAL",0);                                                   //不動小数点型データ TOTALをデフォルト０で渡す
-
-        if (v.getId() ==R.id.button1)                                                     //button1を押した場合EditText1.2の合計を遷移先にtextviewで表示させたい
-            mTextView.setText(String.valueOf(mEditText1) + String.valueOf(mEditText2));
-
-        startActivity(intent); // 指定のActivityを開始
-
+    public void onClick(View v) {                                                             //ボタン１を押した時editText1.2の数値を足して、遷移画面でTOTAL表示
+        Intent intent = new Intent(this, secondActivity.class);                                        //遷移先を指定
+        Double total = 0.0;
+        Double num1 = Double.parseDouble(mEditText1.getText().toString()); // EditText1の入力を小数値に変換
+        Double num2 = Double.parseDouble(mEditText2.getText().toString()); // EditText2の入力を小数値に変換
+        if (v.getId() ==R.id.button1) {
+            total = num1 + num2;
+        }
+        intent.getDoubleExtra("TOTAL", total);
+        startActivity(intent);
     }
 }
