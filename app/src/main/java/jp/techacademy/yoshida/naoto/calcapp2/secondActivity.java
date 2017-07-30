5db3902c0d1378d5c53ff73fbf4968f8b73e84a4
naotoyoshida0 <naotoyoshida16g>
2017-07-30 21:03:50 +0900

    int error

--- app/src/main/java/jp/techacademy/yoshida/naoto/calcapp2/secondActivity.java
+++ app/src/main/java/jp/techacademy/yoshida/naoto/calcapp2/secondActivity.java
@@ -3,23 +3,24 @@ package jp.techacademy.yoshida.naoto.calcapp2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.TextView;
 
+import static jp.techacademy.yoshida.naoto.calcapp2.R.id.textView;
+
+
 public class secondActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+
         Intent intent =getIntent();                            //インテントのオブジェクト取得
-        int value1 = intent.getIntExtra("VALUE1", 0);        //値が存在しない時の初期値
-        int value2 = intent.getIntExtra("VALUE2", 0);
+        Double TOTAL = intent.getDoubleExtra("TOTAL", 0.0);       //遷移データをTOTALという値かつ初期値０で取得
+        TextView textview = (TextView) findViewById(R.id.textView);
+        textView.setText(String.valueOf(TOTAL));
 
-
-
-            TextView textView = (TextView) findViewById(R.id.textView);
-            textView.setText(String.valueOf(value1 + value2));
         }
     }
